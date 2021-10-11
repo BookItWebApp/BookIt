51a81d6f8a4c1cc549107ccc527204ce9dad3e2f
Peter Myer <peter.sr.myer@gmail.com>
2021-10-11 11:38:32 -0500

    Article table added to index.js and exported

--- server/db/index.js
+++ server/db/index.js
@@ -2,5 +2,12 @@ const db = require("./db");
 
 // register models
 require("./models");
+const Article = require('./models/Article')
 
-module.exports = db;
+
+
+module.exports = {
+  db,
+  models: {
+    Article
+  }}
