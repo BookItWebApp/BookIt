601fb9f292ad47ea4f5f055442596b40cd484201
Peter Myer <peter.sr.myer@gmail.com>
2021-10-11 11:26:38 -0500

    Article Model complete

---
+++ server/db/models/Article.js
@@ -0,0 +1,19 @@
+//Articles DB
+const Sequelize = require('sequelize')
+const db = require ('../db')
+
+//Articles must be unique, not empty or null, and must be URL
+const Article = db.define('article', {
+  url: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isUrl: true,
+      notEmpty: true,
+      notNull: true,
+    }
+  }
+})
+
+module.exports = Article
