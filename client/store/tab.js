a92a1c4310864b0a738ae3a167a87b21cdc6cec0
Grace Tveit <grace.tveit@gmail.com>
2021-10-11 12:41:13 -0700

    feat: Adding action  to tabReducer

--- client/store/tab.js
+++ client/store/tab.js
@@ -1,9 +1,18 @@
 // Constants
+const SET_TAB = "SET_TAB"
 // Actions
+const setTab = (tab) => {
+  return {
+    type: SET_TAB,
+    tab
+  }
+}
 // Thunks
 // Reducer
 export default(state = {}, action) => {
   switch (action.type) {
+    case SET_TAB:
+      return action.tab
     default:
       return state;
   }
