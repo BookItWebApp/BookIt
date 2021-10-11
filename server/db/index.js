b7a0596dfadbc54e6c6b27674302bce2ebb9de50
Elena Kot <elenakot07302@gmail.com>
2021-10-11 16:09:30 -0400

    filename fixed in db/index.js

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,7 @@ const db = require("./db");
 
 const User = require("./models/User");
 const Article = require("./models/Article");
-const UserArticle = require("./models/userArticle");
+const UserArticle = require("./models/UserArticle");
 
 //associations could go here!
 
