c7e2b477ca09e57467f072edd9518540aa22a248
Grace Tveit <grace.tveit@gmail.com>
2021-10-11 14:03:05 -0700

    feat: userInfo can now be fetched from the chrome browser

--- client/popup/app.js
+++ client/popup/app.js
@@ -1,20 +1,24 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchTab } from '../store/tab'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUser } from "../store/chromeUser";
+import { fetchTab } from "../store/tab";
 
 export default () => {
-  const tab = useSelector(state => state.tab)
-  const dispatch = useDispatch()
+  const tab = useSelector((state) => state.tab);
+  const user = useSelector((state) => state.chromeUser);
+  const dispatch = useDispatch();
 
   useEffect(async () => {
-    await dispatch(fetchTab())
-  }, [dispatch])
+    dispatch(fetchTab());
+    dispatch(fetchUser());
+    console.log(user);
+  }, [dispatch]);
 
   return (
     <div>
+      <p>Your Email: {user.email}</p>
       <label for="url">URL</label>
       <input name="url" type="text" value={tab.url}></input>
-
     </div>
-  )
-}
+  );
+};
--- client/store/chromeUser.js
+++ client/store/chromeUser.js
@@ -6,9 +6,24 @@ const setUser = (user) => ({
   user,
 });
 // Thunks
+export const fetchUser = () => async (dispatch) => {
+  try {
+    /* 
+    Grabs user info and passes an object with the model of 
+    {email: string, id: string}
+    */
+    await chrome.identity.getProfileUserInfo((userInfo) => {
+      dispatch(setUser(userInfo));
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 // Reducer
 export default (state = {}, action) => {
   switch (action.type) {
+    case SET_USER:
+      return action.user;
     default:
       return state;
   }
