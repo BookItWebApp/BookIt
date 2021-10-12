bbbd9bdc5665738ef44853fd2c1f166c46046d11
Grace Tveit <grace.tveit@gmail.com>
2021-10-12 14:07:06 -0700

    docs: adding info on the tab object grabbed via chrome api

--- client/store/tab.js
+++ client/store/tab.js
@@ -1,30 +1,56 @@
 // Constants
-const SET_TAB = "SET_TAB"
+const SET_TAB = "SET_TAB";
 // Actions
 const setTab = (tab) => {
   return {
     type: SET_TAB,
-    tab
-  }
-}
+    tab,
+  };
+};
 // Thunks
 export const fetchTab = () => {
   return async (dispatch) => {
+    /*
+      Tab item model:
+      {
+        active: boolean
+        audible; boolean
+        autoDiscardable: boolean
+        discarded: boolean
+        favIconUrl: string
+        groupId: int
+        height: int
+        highlighted: boolean
+        id: int
+        incognito: boolean
+        index: int
+        mutedInfo: {
+          muted: boolean
+        }
+        pinned: boolean
+        selected: boolean
+        status: string(?)
+        title: string
+        url: string
+        width: int
+        windowId: int
+      }
+    */
     try {
-      let queryOptions = { active: true, currentWindow: true}
-      let [tab] = await chrome.tabs.query(queryOptions)
-      dispatch(setTab(tab))
+      let queryOptions = { active: true, currentWindow: true };
+      let [tab] = await chrome.tabs.query(queryOptions);
+      dispatch(setTab(tab));
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-}
+  };
+};
 // Reducer
-export default(state = {}, action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case SET_TAB:
-      return action.tab
+      return action.tab;
     default:
       return state;
   }
-}
+};
