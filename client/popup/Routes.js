bb8560f9a731b2e0535c895d7cb6dba470e92677
Elena Kot <elenakot07302@gmail.com>
2021-10-12 17:01:54 -0400

    popup routes file added

---
+++ client/popup/Routes.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch } from "react-router-dom";
+import { Login } from "./popup";
+import { CreateArticle } from "./popup";
+import { me } from "./store";
+
+/**
+ * COMPONENT
+ */
+class Routes extends Component {
+  componentDidMount() {
+    this.props.loadInitialData();
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+
+    return (
+      <div>
+        {isLoggedIn ? (
+          <Switch>
+            <Route path="/createArticle" component={CreateArticle} />
+          </Switch>
+        ) : (
+          <Switch>
+            <Route path="/login" component={Login} />
+          </Switch>
+        )}
+      </div>
+    );
+  }
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = (state) => {
+  return {
+    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+    isLoggedIn: !!state.auth.id,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    loadInitialData() {
+      dispatch(me());
+    },
+  };
+};
+
+// The `withRouter` wrapper makes sure that updates are not blocked
+// when the url changes
+export default withRouter(connect(mapState, mapDispatch)(Routes));
