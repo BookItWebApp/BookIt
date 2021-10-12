80f3f56c6fa9cbb428168fb8aeafd1050bdbd2ce
Peter Myer <peter.sr.myer@gmail.com>
2021-10-12 11:58:32 -0500

    userarticle for get one, get all, and post one. Additionally, a component exists through get one route that will render articles to /articles page.

--- server/api/userArticles.js
+++ server/api/userArticles.js
@@ -1,17 +1,49 @@
 //ROUTES FOR A SINGLE USERS ARTICLES
 const router = require("express").Router();
-const {models: { UserArticle },} = require("../db");
+const {
+  models: { UserArticle, Article },
+} = require("../db");
 
 module.exports = router;
 
-//Retrieve all of a user's articles
+//Retrieve all articles from UserArticle Table
 router.get("/", async (req, res, next) => {
   try {
-    const  id  = req.query.userId;
-    const articles = await UserArticle.findAllByUser(id);
-    res.json(articles);
+    const allArticles = await UserArticle.findAll({
+      attributes: ["featured", "name", "readAt", "createdAt"],
+      include: {
+        model: Article,
+        attributes: ["id","url"],
+      }})
+    res.json(allArticles)
   } catch (err) {
     next(err);
   }
 });
 
+//Retrieve all of a single user's articles
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const userArticles = await UserArticle.findAllByUser(id);
+    res.json(userArticles);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Add new article UserArticle Table
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, userId, articleId, featured } = req.body;
+    const userArticles = await UserArticle.create({
+      name: name,
+      userId: userId,
+      articleId: articleId,
+      featured: featured,
+    });
+    res.json(userArticles);
+  } catch (err) {
+    next(err);
+  }
+});
