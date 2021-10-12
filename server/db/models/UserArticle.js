fecb56f642b90c3dbe8fe80901e2c6ce173391d7
Peter Myer <peter.sr.myer@gmail.com>
2021-10-12 14:03:07 -0500

    update database to include tags and tag names

--- server/db/models/UserArticle.js
+++ server/db/models/UserArticle.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
-const Article = require("../models/Article")
+const Article = require("../models/Article");
+const Tagging = require("../models/Tagging");
+const Tags = require("../models/Tag");
 
 const UserArticle = db.define(
   "userArticle",
@@ -45,11 +47,19 @@ const UserArticle = db.define(
 //Find all articles belonging to a particular user
 UserArticle.findAllByUser = function (currentUserId) {
   return this.findAll({
-    attributes: ["featured", "name", "readAt", "createdAt"],
     where: { userId: currentUserId },
     include: {
       model: Article,
-      attributes: ["id","url"],
+      as: "Article Url",
+      attributes: ["id", "url"],
+      include: {
+        model: Tagging,
+        as: " Article Tags",
+        include: {
+          model: Tags,
+          as: "Tag Names"
+        }
+      },
     },
   });
 };
