4695adb22df18fb69da9966b5950c7227001fae8
Peter Myer <peter.sr.myer@gmail.com>
2021-10-13 12:41:03 -0500

    added SingleArticle component for rendering each article on homepage

--- client/popup/app.js
+++ client/popup/app.js
@@ -1,20 +1,12 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchTab } from '../store/tab'
+import React from "react";
+import Routes from "./Routes";
 
-export default () => {
-  const tab = useSelector(state => state.tab)
-  const dispatch = useDispatch()
-
-  useEffect(async () => {
-    await dispatch(fetchTab())
-  }, [dispatch])
-
+const App = () => {
   return (
     <div>
-      <label for="url">URL</label>
-      <input name="url" type="text" value={tab.url}></input>
-
+      <Routes />
     </div>
-  )
-}
+  );
+};
+
+export default App;
