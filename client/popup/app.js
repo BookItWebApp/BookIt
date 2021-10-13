0126847f91211a0290e1e1501a5f464fbcb00d66
Peter Myer <peter.sr.myer@gmail.com>
2021-10-13 12:00:25 -0500

    merge on main

--- client/popup/app.js
+++ client/popup/app.js
@@ -1,12 +1,20 @@
-import React from "react";
-import Routes from "./Routes";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchTab } from '../store/tab'
 
-const App = () => {
+export default () => {
+  const tab = useSelector(state => state.tab)
+  const dispatch = useDispatch()
+
+  useEffect(async () => {
+    await dispatch(fetchTab())
+  }, [dispatch])
+
   return (
     <div>
-      <Routes />
-    </div>
-  );
-};
+      <label for="url">URL</label>
+      <input name="url" type="text" value={tab.url}></input>
 
-export default App;
+    </div>
+  )
+}
