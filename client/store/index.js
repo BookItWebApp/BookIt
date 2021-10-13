72aa79c7994f38975c6f074a04e87d453ea0bbe9
Grace Tveit <grace.tveit@gmail.com>
2021-10-13 09:35:43 -0700

    fix: Syntax error at client/store/index.js

--- client/store/index.js
+++ client/store/index.js
@@ -1,17 +1,17 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
-import tab from './tab'
-import userArticlesReducer from './userArticles'
-import tagsReducer from './tag'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import auth from './auth';
+import tab from './tab';
+import userArticlesReducer from './userArticles';
+import tagsReducer from './tag';
 
 const reducer = combineReducers({
   auth,
   tab,
   userArticles: userArticlesReducer,
-  tags: tagsReducer })
+  tags: tagsReducer,
 });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
