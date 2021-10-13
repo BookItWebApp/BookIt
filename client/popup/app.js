86152418fd69e58a081cf3d81bc0fde5506557ce
Peter Myer <peter.sr.myer@gmail.com>
2021-10-12 19:58:22 -0500

    chrome extension now posts to localhost

--- client/popup/app.js
+++ client/popup/app.js
@@ -1,20 +1,27 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchTab } from '../store/tab'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTab } from "../store/tab";
 
 export default () => {
-  const tab = useSelector(state => state.tab)
-  const dispatch = useDispatch()
+  const tab = useSelector((state) => state.tab);
+  const dispatch = useDispatch();
 
   useEffect(async () => {
-    await dispatch(fetchTab())
-  }, [dispatch])
+    await dispatch(fetchTab());
+  }, [dispatch]);
 
   return (
     <div>
-      <label for="url">URL</label>
-      <input name="url" type="text" value={tab.url}></input>
-
+      <label htmlFor="url">URL</label>
+      <form
+        name="url"
+        action="http://localhost:8080/api/articles/"
+        method="POST"
+        target="_self"
+      >
+        <input type="url" type="text" name='url' value={tab.url}></input>
+        <input type="submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
