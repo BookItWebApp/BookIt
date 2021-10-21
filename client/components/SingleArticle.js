61c78ad38f6b87c6b1fab2c9189c77e88ee25ce8
Grace Tveit <grace.tveit@gmail.com>
2021-10-21 14:46:38 -0700

    fix: Marking an article as read no longer removes authors

--- client/components/SingleArticle.js
+++ client/components/SingleArticle.js
@@ -5,69 +5,65 @@ import { previewArticle } from "../store/SingleArticle";
 import { markUserArticle } from "../store/userArticles";
 
 export function SingleArticle(props) {
-    const dispatch = useDispatch();
-    const articleMetaData = useSelector((state) => state.metaData);
-    const user = useSelector((state) => state.auth);
-    const article = props.article;
-    const taggings = article.taggings;
+  const dispatch = useDispatch();
+  const articleMetaData = useSelector((state) => state.metaData);
+  const user = useSelector((state) => state.auth);
+  const article = props.article;
+  const taggings = article.taggings;
 
-    console.log("article", article.article);
-    // useEffect(() => {
-    //   dispatch(previewArticle(article.article.url, article.id));
-    // }, [dispatch]);
+  console.log('article', article.article);
+  // useEffect(() => {
+  //   dispatch(previewArticle(article.article.url, article.id));
+  // }, [dispatch]);
 
-    function markAsCompleted() {
-        // alert("trying to complete item with an id of " + user.id);
-        article.readAt = new Date().toISOString();
-        console.log("MARK AS REEAD > ", article);
-        dispatch(markUserArticle(user.id, article));
-    }
-    //
-    return (
-        <div className="single-article--container">
-            <a href={article.url}>
-                <span className="title-name--single-article">
-                    Title: {article.name}
-                </span>
-                <br />
-            </a>
-            Private: {article.isPrivate ? "True" : "False"}
-            <br />
-            Tags:
-            {taggings.map((tagging, idx) => {
-                return <span key={idx.toString()}> {tagging.tag.name}</span>;
-            })}
-            <br />
-            {/* {article.article.authors && article.article.authors.length > 0 ? (
-                article.article.authors.length > 1 ? (
-                    <ul>
-                        By:
-                        {article.article.authors.map((author) => (
-                            <li key={author.id}>
-                                <NavLink to={`/authors/${author.id}`}>
-                                    {author.name}
-                                </NavLink>
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <span>
-                        By:{" "}
-                        <NavLink
-                            to={`/authors/${article.article.authors[0].id}`}
-                        >
-                            {article.article.authors[0].name}
-                        </NavLink>
-                        <button onClick={markAsCompleted}>mark</button>
-                    </span>
-                )
-            ) : (
-                <React.Fragment />
-            )} */}
-            <span>
-                <button onClick={markAsCompleted}>mark</button>
-            </span>
-            <span>{article.readAt ? "Read" : "Unread"}</span>
-        </div>
-    );
+  function markAsCompleted() {
+    // alert("trying to complete item with an id of " + user.id);
+    article.readAt = new Date().toISOString();
+    console.log('MARK AS REEAD > ', article);
+    dispatch(markUserArticle(user.id, article));
+  }
+  //
+  return (
+    <div className="single-article--container">
+      <a href={article.url}>
+        <span className="title-name--single-article">
+          Title: {article.name}
+        </span>
+        <br />
+      </a>
+      Private: {article.isPrivate ? 'True' : 'False'}
+      <br />
+      Tags:
+      {taggings.map((tagging, idx) => {
+        return <span key={idx.toString()}> {tagging.tag.name}</span>;
+      })}
+      <br />
+      {article.article.authors && article.article.authors.length > 0 ? (
+        article.article.authors.length > 1 ? (
+          <ul>
+            By:
+            {article.article.authors.map((author) => (
+              <li key={author.id}>
+                <NavLink to={`/authors/${author.id}`}>{author.name}</NavLink>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <span>
+            By:{' '}
+            <NavLink to={`/authors/${article.article.authors[0].id}`}>
+              {article.article.authors[0].name}
+            </NavLink>
+            <button onClick={markAsCompleted}>mark</button>
+          </span>
+        )
+      ) : (
+        <React.Fragment />
+      )}
+      <span>
+        <button onClick={markAsCompleted}>mark</button>
+      </span>
+      <span>{article.readAt ? 'Read' : 'Unread'}</span>
+    </div>
+  );
 }
