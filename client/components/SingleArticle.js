1e6d22917842e96e3ce19d0ca12f54a59a47d3b6
Peter Myer <peter.sr.myer@gmail.com>
2021-10-21 12:28:10 -0500

    added author check >=1 to avoid edge case where author array is empty

--- client/components/SingleArticle.js
+++ client/components/SingleArticle.js
@@ -1,14 +1,13 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from 'react-router-dom'
-import { previewArticle } from "../store/SingleArticle";
-import { markUserArticle } from "../store/userArticles";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { previewArticle } from '../store/SingleArticle';
+import { markUserArticle } from '../store/userArticles';
 
 export function SingleArticle(props) {
-    const dispatch = useDispatch();
-    const articleData = useSelector((state) => state.metaData);
-    const user = useSelector((state) => state.auth);
-
+  const dispatch = useDispatch();
+  const articleMetaData = useSelector((state) => state.metaData);
+  const user = useSelector((state) => state.auth);
   const article = props.article;
   const taggings = article.taggings;
 
@@ -16,29 +15,30 @@ export function SingleArticle(props) {
     dispatch(previewArticle(article.article.url, article.id));
   }, [dispatch]);
 
-    function markAsCompleted() {
-        // alert("trying to complete item with an id of " + user.id);
-        article.readAt = new Date().toISOString();
-        console.log("MARK AS REEAD > ", article);
-        dispatch(markUserArticle(user.id, article));
-    }
-    //
-    return (
-        <div className="single-article--container">
-            <a href={article.url}>
-                <span className="title-name--single-article">
-                    Title: {article.name}
-                </span>
-                <br />
-            </a>
-            Private: {article.isPrivate ? "True" : "False"}
-            <br />
-            Tags:
-            {taggings.map((tagging, idx) => {
-                return <span key={idx.toString()}> {tagging.tag.name}</span>;
-            })}
-            <br />
-{article.article.authors ? (
+  function markAsCompleted() {
+    // alert("trying to complete item with an id of " + user.id);
+    article.readAt = new Date().toISOString();
+    console.log('MARK AS REEAD > ', article);
+    dispatch(markUserArticle(user.id, article));
+  }
+  //
+  console.log(article)
+  return (
+    <div className="single-article--container">
+      <a href={article.url}>
+        <span className="title-name--single-article">
+          Title: {article.name}
+        </span>
+        <br />
+      </a>
+      Private: {article.isPrivate ? 'True' : 'False'}
+      <br />
+      Tags:
+      {taggings.map((tagging, idx) => {
+        return <span key={idx.toString()}> {tagging.tag.name}</span>;
+      })}
+      <br />
+      {article.article.authors.length >=1 ? (
         article.article.authors.length > 1 ? (
           <ul>
             By:
@@ -59,11 +59,11 @@ export function SingleArticle(props) {
       ) : (
         <React.Fragment />
       )}
-            <span>
-                <button onClick={markAsCompleted}>mark</button>
-                <button>share</button>
-            </span>
-            <span>{article.readAt ? "Read" : "Unread"}</span>
-        </div>
-    );
+      <span>
+        <button onClick={markAsCompleted}>mark</button>
+        <button>share</button>
+      </span>
+      <span>{article.readAt ? 'Read' : 'Unread'}</span>
+    </div>
+  );
 }
