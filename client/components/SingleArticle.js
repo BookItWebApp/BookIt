cd72ac722ffb84389d96a3a48e04db6588a3c056
Grace Tveit <grace.tveit@gmail.com>
2021-10-22 16:11:30 -0700

    fix: re-adding 'add author' button hide feature

--- client/components/SingleArticle.js
+++ client/components/SingleArticle.js
@@ -11,46 +11,49 @@ export function SingleArticle(props) {
     const article = props.article;
     const taggings = article.taggings;
 
-    console.log("article", article.article);
+    const articleList = article.article.authors.map((author) => author.id);
+    console.log(articleList);
+
+    console.log('article', article.article);
     // useEffect(() => {
     //   dispatch(previewArticle(article.article.url, article.id));
     // }, [dispatch]);
 
     function markAsCompleted() {
-        article.readAt = new Date().toISOString();
-        dispatch(markUserArticle(user.id, article));
+      article.readAt = new Date().toISOString();
+      dispatch(markUserArticle(user.id, article));
     }
 
     function deleteBookmark() {
-        console.log("DELTE", user.id, article);
-        dispatch(deleteProduct(article.id, article));
+      console.log('DELTE', user.id, article);
+      dispatch(deleteProduct(article.id, article));
     }
     //
     return (
-        <div className="single-article--container">
-            <a href={article.url}>
-                <span className="title-name--single-article">
-                    Title: {article.name}
-                </span>
-                <br />
-            </a>
-            {user.author ? (
-              <div className="addAuthorButton">
-                <button onClick={() => props.addAuthor(article.article.id)}>
-                  I wrote this!
-                </button>
-              </div>
-            ) : (
-              <React.Fragment />
-            )}
-            Private: {article.isPrivate ? "True" : "False"}
-            <br />
-            Tags:
-            {taggings.map((tagging, idx) => {
-                return <span key={idx.toString()}> {tagging.tag.name}</span>;
-            })}
-            <br />
-            {article.article.authors ? (
+      <div className="single-article--container">
+        <a href={article.url}>
+          <span className="title-name--single-article">
+            Title: {article.name}
+          </span>
+          <br />
+        </a>
+        {user.author && !articleList.includes(user.author.id) ? (
+          <div className="addAuthorButton">
+            <button onClick={() => props.addAuthor(article.article.id)}>
+              I wrote this!
+            </button>
+          </div>
+        ) : (
+          <React.Fragment />
+        )}
+        Private: {article.isPrivate ? 'True' : 'False'}
+        <br />
+        Tags:
+        {taggings.map((tagging, idx) => {
+          return <span key={idx.toString()}> {tagging.tag.name}</span>;
+        })}
+        <br />
+        {article.article.authors ? (
           article.article.authors.length > 1 ? (
             <ul>
               By:
@@ -71,13 +74,13 @@ export function SingleArticle(props) {
         ) : (
           <React.Fragment />
         )}
-            <span>
-                <button onClick={markAsCompleted}>mark</button>
-            </span>
-            <span>{article.readAt ? "Read" : "Unread"}</span>
-            <span>
-                <button onClick={deleteBookmark}>delete</button>
-            </span>
-        </div>
+        <span>
+          <button onClick={markAsCompleted}>mark</button>
+        </span>
+        <span>{article.readAt ? 'Read' : 'Unread'}</span>
+        <span>
+          <button onClick={deleteBookmark}>delete</button>
+        </span>
+      </div>
     );
 }
