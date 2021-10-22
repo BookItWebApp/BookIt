f27e631a5ee622059f5e482bb1cd386126ac0dd8
Grace Tveit <grace.tveit@gmail.com>
2021-10-22 10:30:46 -0700

    feat: Adding an addAuthor button

--- client/components/SingleArticle.js
+++ client/components/SingleArticle.js
@@ -9,61 +9,67 @@ export function SingleArticle(props) {
     const articleData = useSelector((state) => state.metaData);
     const user = useSelector((state) => state.auth);
 
-  const article = props.article;
-  const taggings = article.taggings;
 
-  useEffect(() => {
-    dispatch(previewArticle(article.article.url, article.id));
-  }, [dispatch]);
+    const article = props.article;
+    const taggings = article.taggings;
+
+    useEffect(() => {
+      dispatch(previewArticle(article.article.url, article.id));
+    }, [dispatch]);
 
     function markAsCompleted() {
-        // alert("trying to complete item with an id of " + user.id);
-        article.readAt = new Date().toISOString();
-        console.log("MARK AS REEAD > ", article);
-        dispatch(markUserArticle(user.id, article));
+      // alert("trying to complete item with an id of " + user.id);
+      article.readAt = new Date().toISOString();
+      console.log('MARK AS REEAD > ', article);
+      dispatch(markUserArticle(user.id, article));
     }
     //
     return (
-        <div className="single-article--container">
-            <a href={article.url}>
-                <span className="title-name--single-article">
-                    Title: {article.name}
-                </span>
-                <br />
-            </a>
-            Private: {article.isPrivate ? "True" : "False"}
-            <br />
-            Tags:
-            {taggings.map((tagging, idx) => {
-                return <span key={idx.toString()}> {tagging.tag.name}</span>;
-            })}
-            <br />
-{article.article.authors ? (
-        article.article.authors.length > 1 ? (
-          <ul>
-            By:
-            {article.article.authors.map((author) => (
-              <li key={author.id}>
-                <NavLink to={`/authors/${author.id}`}>{author.name}</NavLink>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <span>
-            By:{' '}
-            <NavLink to={`/authors/${article.article.authors[0].id}`}>
-              {article.article.authors[0].name}
-            </NavLink>
+      <div className="single-article--container">
+        <a href={article.url}>
+          <span className="title-name--single-article">
+            Title: {article.name}
           </span>
-        )
-      ) : (
-        <React.Fragment />
-      )}
-            <span>
-                <button onClick={markAsCompleted}>mark</button>
-                <button>share</button>
-            </span>
-            <span>{article.readAt ? "Read" : "Unread"}</span>
+          <br />
+        </a>
+        <div className="addAuthorButton">
+          <button onClick={() => props.addAuthor(article.article.id)}>
+            I wrote this!
+          </button>
         </div>
+        Private: {article.isPrivate ? 'True' : 'False'}
+        <br />
+        Tags:
+        {taggings.map((tagging, idx) => {
+          return <span key={idx.toString()}> {tagging.tag.name}</span>;
+        })}
+        <br />
+        {article.article.authors ? (
+          article.article.authors.length > 1 ? (
+            <ul>
+              By:
+              {article.article.authors.map((author) => (
+                <li key={author.id}>
+                  <NavLink to={`/authors/${author.id}`}>{author.name}</NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <span>
+              By:{' '}
+              <NavLink to={`/authors/${article.article.authors[0].id}`}>
+                {article.article.authors[0].name}
+              </NavLink>
+            </span>
+          )
+        ) : (
+          <React.Fragment />
+        )}
+        <span>
+          <button onClick={markAsCompleted}>mark</button>
+          <button>share</button>
+        </span>
+        <span>{article.readAt ? 'Read' : 'Unread'}</span>
+      </div>
     );
 }
