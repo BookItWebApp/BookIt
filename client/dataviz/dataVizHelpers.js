1ccde1834da48c567eb19662b736e42f47d4d9b5
Peter Myer <peter.sr.myer@gmail.com>
2021-10-28 15:40:04 -0500

    cleaned and commented dataviz indicator component

--- client/dataviz/dataVizHelpers.js
+++ client/dataviz/dataVizHelpers.js
@@ -1,12 +1,14 @@
-//This file contains helper functions for data cleaner
-
+//This file contains shared helper functions for data cleaning
 const { DateTime } = require('luxon');
 
 //USER ARTICLE HELPER FUNCTIONS
-  //Read Articles Per Date
+
   const dateList=[]
   const sortedArticles = {};
 
+//Read Articles Sorted
+///Returns object with keys made of sorted readDates and list of articles
+///read that day
   export function readArticlesDates(userArticles){
     const readArticles = userArticles.filter(
     (article) => article.readAt !== null
